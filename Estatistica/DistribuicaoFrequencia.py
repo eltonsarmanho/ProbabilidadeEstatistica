053742dcaf5e60231ce7124adc99b656f9f4219c
eltonsarmanho <s0@19t2&>
2021-08-07 16:09:43 -0300

    OK

--- Estatistica/DistribuicaoFrequencia.py
+++ Estatistica/DistribuicaoFrequencia.py
@@ -4,18 +4,19 @@ if __name__ == '__main__':
     df = [3.2,4.1,4.9 , 5.0 , 7.3 , 6.7 , 6.6 , 7.4 , 7.1 , 4.0 , 5.5 , 5.4 , 6.5 , 6.5 , 7.1 , 5.2 , 8.3 , 5.7 , 6.8,  6.4]
     df.sort()
     #Ordena os elementos
-    print(df)
+    #print(df)
 
     n = len(df);
-    print("Valor de n é %s"  % n)
+    #print("Valor de n é %s" % n)
+
     # Amplitude dos dados = Valor maior dos registros - menor valor
-    indice_maior = n-1
+    indice_maior = n - 1
     indice_menor = 0
     at = df[indice_maior] - df[indice_menor]
-    if(n > 25):
-        # Lembrando que k = raiz quadrada do total de registros/amostras
+    if (n > 25):
         k = math.sqrt(n)
-    else: k = 1+ 3.22*math.log(n)
+    else:
+        k = 1 + 3.22 * math.log(n)
 
     # O valor de amplitude de classe pode ser arredondado para um número inteiro, geralmente para facilitar a interpretação da tabela.
     h = at / k
@@ -32,8 +33,11 @@ if __name__ == '__main__':
         frequencias.append('{} - {}'.format(round(valor, 1), round(valor + h, 1)))
         valor += h
 
+
     # Discretização dos valores em k faixas, rotuladas pela lista criada anteriormente
-    freq_abs = pd.qcut(df, len(frequencias),
-                       labels=frequencias)
-    #print(pd.value_counts(freq_abs,sort=False,normalize=True))
-    print(pd.value_counts(freq_abs, sort=False))
+    freq_abs = pd.qcut(df, len(frequencias), labels=frequencias)
+    print(freq_abs)
+    print(pd.value_counts(freq_abs,sort=False,normalize=True))
+    #print(pd.value_counts(freq_abs, sort=False))
+
+
---
+++ Estatistica/Questao5.py
@@ -0,0 +1,28 @@
+import itertools
+from collections import Counter
+import matplotlib.pyplot as plt
+import numpy as np
+if __name__ == '__main__':
+    a_0 = [0]*25
+    a_1 = [1] * 20
+    a_2 = [2] * 3
+    a_3 = [3] * 1
+    a_4 = [4] * 1
+    a= []
+    a.extend(a_0)
+    a.extend(a_1)
+    a.extend(a_2)
+    a.extend(a_3)
+    a.extend(a_4)
+    recounted = Counter(a)
+    print(recounted)
+    n, bins, patches = plt.hist(x=a, bins=5, color='#0504aa',
+                                alpha=0.7, rwidth=0.85,)
+    plt.grid(axis='y', alpha=0.75)
+    plt.xlabel('Valor')
+    xticks = [(bins[idx + 1] + value) / 2 for idx, value in enumerate(bins[:-1])]
+    plt.xticks(xticks,labels = ["0","1","2","3","4"])
+    plt.ylabel('Frequencia')
+    plt.title('Histograma')
+   # plt.margins(x=0.005) # less padding left and right
+    plt.show()
