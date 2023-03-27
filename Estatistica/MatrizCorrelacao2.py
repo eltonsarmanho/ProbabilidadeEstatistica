05644425c653e2e290298f4fc44ca477a977217a
eltonsarmanho <s0@19t2&>
2023-03-27 17:00:07 -0300

    Matriz Correlacao update

--- Estatistica/MatrizCorrelacao2.py
+++ Estatistica/MatrizCorrelacao2.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('../Dados/iris.csv')
 print(df.describe())
 
+df = df[['sepal.length','sepal.width','petal.length']]
 corrMatrix = df.corr()
 sn.heatmap(corrMatrix, annot=True)
 plt.show()
